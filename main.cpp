c79a4b27041ab226c4cc8a0c1c8ef897e190bc3f
Water Chika <water_chika@outlook.com>
2024-05-02 21:27:16 +0800

    Make all operation is normalized and add some operations.

--- main.cpp
+++ main.cpp
@@ -3,14 +3,6 @@
 
 using namespace water::rational_number;
 
-auto& operator<<(std::ostream& out, const concept_helper::rational_number auto& q) {
-	if (q.denominator() == 1) {
-		return out << q.numerator();
-	}
-	else {
-		return out << "(" << q.numerator() << "/" << q.denominator() << ")";
-	}
-}
 
 int main() {
 	using Q = water::rational_number::rational_number<int32_t,uint32_t>;
